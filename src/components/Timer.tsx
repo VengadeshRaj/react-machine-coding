f8ed1f2b8219bf55ee36710a6d4c216d6e7fcb79
vengadeshraj <vengadeshraj405@gmail.com>
2025-09-20 23:18:11 +0530

    timer functionality added

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Timer = () => {
   const [timerValue, setTimerValue] = useState(0);
-  const intervalId = null;
+  const timerStarted = Boolean(timerValue);
+  const intervalRef: any = useRef(null);
 
-  const startClick = () => {};
+  const startClick = () => {
+    intervalRef.intervalId = setInterval(() => {
+      setTimerValue((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const stopClick = () => {
+    if (intervalRef.intervalId) clearInterval(intervalRef.intervalId);
+  };
+
+  const resetClick = () => {
+    if (intervalRef.intervalId) clearInterval(intervalRef.intervalId);
+    setTimerValue(0);
+  };
 
   return (
     <div>
       <h1>{timerValue}</h1>
-      <button>Start</button>
-      <button disabled={!!!timerValue}>Stop</button>
-      <button disabled={!!!timerValue}>reset</button>
+      <button onClick={() => startClick()}>Start</button>
+      <button disabled={!timerStarted} onClick={() => stopClick()}>
+        Stop
+      </button>
+      <button disabled={!timerStarted} onClick={() => resetClick()}>reset</button>
     </div>
   );
 };
