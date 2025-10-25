55a2c25290f62e7ddaa0784ca3348029da4db06d
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-26 00:41:01 +0530

    to-do-list style update

--- to-do-list/src/components/NoItemsCard.tsx
+++ to-do-list/src/components/NoItemsCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NoItemsCard = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 p-10 w-full h-[300px] bg-white rounded">
+    <div className="flex flex-col items-center justify-center gap-5 p-10 w-full h-[300px] bg-gray-100 rounded">
       <span className="text-gray-300">--No Items--</span>
     </div>
   );
