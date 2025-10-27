9691d96c0d0c6d4a5bf40d48fba5fbb92a59bbc0
vengadeshraj <vengadeshraj405@gmail.com>
2025-10-27 11:29:59 +0530

    Traffic light topic added

---
+++ traffic-light/src/TrafficLight.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrafficLight = () => {
+  return <div className="flex flex-col items-center">Hellow from Traffic light!</div>;
+};
+
+export default TrafficLight;
