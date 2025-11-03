59b8a3d24b6c830f5a3ebab37ab9e3800b0d8af6
vengadeshraj <vengadeshraj405@gmail.com>
2025-11-03 22:39:20 +0530

    nested comment section modified

--- nested-comment-section/src/CommentSection.tsx
+++ nested-comment-section/src/CommentSection.tsx
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Comment from "./components/Comment";
 import Button from "./components/Button";
 
+interface Comment {
+  index: number;
+  commentMessage: string;
+  vote: number;
+  date: number;
+  time: number;
+}
+
+export interface Comments extends Comment{
+  replies: Comment[];
+};
+
 const NestedCommentSection = () => {
+  const [newComment, setNewComment] = useState("");
+  const [comments, setComments] = useState<Comments[]>([]);
+
+  const addCommentClick = () => {
+    if (newComment) {
+      setComments((prev) => [
+        ...prev,
+        {
+          index: prev.length,
+          commentMessage: newComment,
+          vote: 0,
+          date: new Date().getDate(),
+          time: new Date().getTime(),
+          replies: [],
+        },
+      ]);
+      setNewComment("");
+    }
+  };
+
   return (
     <div className="container">
       <div>
         <h1>Nested Comment System</h1>
         <div className="input-sec-container">
-          <input type="text" className="comment-input" />
-          <Button text="Add Comment" />
+          <input
+            type="text"
+            className="comment-input"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <Button text="Add Comment" onClick={addCommentClick} />
         </div>
       </div>
       <div>
-        <Comment />
+        {comments.map((c, index) => (
+          <Comment
+            comments= {comments}
+            comment={c.commentMessage}
+            votes={c.vote}
+            date={c.date}
+            time={c.time}
+            index={index}
+            setComments={setComments}
+          />
+        ))}
       </div>
     </div>
   );
--- nested-comment-section/src/components/Comment.tsx
+++ nested-comment-section/src/components/Comment.tsx
@@ -1,24 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
-import '../styles/comment.css'
+import "../styles/comment.css";
+import { Comments } from "../CommentSection";
 
-const Comment = () => {
+type CommentProps = {
+  comments: Comments[];
+  comment: string;
+  setComments: any;
+  votes: number;
+  date: number;
+  time: number;
+  index: number;
+};
+
+const Comment = (props: CommentProps) => {
+  const { comment, votes, date, time, index, comments, setComments } = props;
+  const [replyComment, setReplyComment] = useState("");
+  const [isReplyEnabled, setIsReplyEnabled] = useState(false);
+  const [isEditEnabled, setIsEditEnabled] = useState(false);
+
+  const replyClick = () => {
+    setIsEditEnabled(false);
+    setIsReplyEnabled(true);
+  };
+
+  const editClick = () => {
+    setIsEditEnabled(true);
+    setIsReplyEnabled(false);
+  };
+
+  const submitReplyClick = () => {
+    const updatedComments = comments.map((data, i) => {
+      if (i == index) {
+        data.replies = [
+          ...data.replies,
+          {
+            index: data.replies.length,
+            commentMessage: replyComment,
+            vote: 0,
+            date: new Date().getDate(),
+            time: new Date().getTime(),
+          },
+        ];
+      }
+
+      return data;
+    });
+    setReplyComment("");
+    setIsReplyEnabled(false);
+    setComments([...updatedComments]);
+  };
+
   return (
     <div className="comment-container">
       <div>
-        This is sample comment
-        <div>Votes : 5</div>
-        <div>03/03/2025, 03:00 PM</div>
+        {comment}
+        <div>Votes : {votes}</div>
+        <div>
+          {date}, {time}
+        </div>
         <div className="comment-btn-container">
           <Button text={"👍"} />
           <Button text={"👎"} />
-          <Button text="Reply" />
-          <Button text={"Edit"} />
+          <Button text="Reply" onClick={replyClick} />
+          <Button text={"Edit"} onClick={editClick} />
           <Button text={"Delete"} />
         </div>
       </div>
-      <input className="reply-comment-input"/>
-      <Button text="reply"/>
+      {isReplyEnabled && (
+        <>
+          <input
+            className="reply-comment-input"
+            value={replyComment}
+            onChange={(e) => setReplyComment(e.target.value)}
+          />
+          <Button text="Submit Reply" onClick={submitReplyClick} />
+        </>
+      )}
+      {isEditEnabled && (
+        <div>
+          <input className="reply-comment-input" />
+          <Button text="Update Comment" />
+        </div>
+      )}
     </div>
   );
 };
