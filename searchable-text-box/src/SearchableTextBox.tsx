299e9412f58a5feb7edecb08cb0fc14ab6da93d4
vengadeshraj <vengadeshraj405@gmail.com>
2025-11-23 23:05:51 +0530

    new topics searchable txt box added

---
+++ searchable-text-box/src/SearchableTextBox.tsx
@@ -0,0 +1,8 @@
+export default function SearchableTextBox(){
+    return(
+        <div className="flex flex-col gap-5 items-center h-screen justify-center">
+            <h1>Search any fruit</h1>
+            <input/>
+        </div>
+    )
+}
