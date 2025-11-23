f5c08eb2f96ef40bbcfe7212d2a8540d5eeb13a5
vengadeshraj <vengadeshraj405@gmail.com>
2025-11-23 23:22:03 +0530

    Searchable txt dev started

--- searchable-text-box/src/SearchableTextBox.tsx
+++ searchable-text-box/src/SearchableTextBox.tsx
@@ -1,8 +1,16 @@
-export default function SearchableTextBox(){
-    return(
-        <div className="flex flex-col gap-5 items-center h-screen justify-center">
-            <h1>Search any fruit</h1>
-            <input/>
-        </div>
-    )
+export default function SearchableTextBox() {
+  return (
+    <div className="flex flex-col gap-5 items-center h-screen justify-center bg-gray-200">
+      <h1 className="font-bold text-3xl text-orange-500">
+        Search Any Fruit! 🍍🥭🍎🍉
+      </h1>
+      <div>
+        <input className="border border-gray-800 w-[300px] p-1 rounded" />
+        <ul className="bg-white p-2 mt-1 bg-gray-100">
+          <li className="focus:bg-blue-400 p-2 border">Test</li>
+          <li>Test</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
