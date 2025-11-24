79cd4c66adbe9b5b68b99a53105bd726a97dc45c
Vengadesh Raj <vengadeshraj405@gmail.com>
2025-11-24 12:29:01 +0530

    Searchable text box dev complete (#4)
    * new topics searchable txt box added
    
    * Searchable txt dev started
    
    * searchable text box dev complete
    
    * searchable text box enhancement added

--- searchable-text-box/README.md
+++ searchable-text-box/README.md
@@ -1,2 +1,11 @@
 # Searchable Text Box
 
+A searchable text box that allows users to look up fruits, displaying dynamic suggestions based on their input. The component uses a debouncing mechanism to minimize unnecessary event triggers and improve performance.
+
+![searchable-text-box](./images/searchable-text-box.png)
+
+## Technologies Used:
+
+- React JS
+- Tailwind CSS
+- Debouncing mechanism
--- searchable-text-box/src/SearchableTextBox.tsx
+++ searchable-text-box/src/SearchableTextBox.tsx
@@ -1,15 +1,54 @@
+import { useRef, useState } from "react";
+import { FRUITS } from "./constants";
+
 export default function SearchableTextBox() {
+  const [searchInput, setSearchInput] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const timerRef: any = useRef({});
+
+  const textBoxOnChange = (newValue: string) => {
+    setSearchInput(newValue);
+    if (timerRef?.current?.timerId) clearTimeout(timerRef.current.timerId);
+
+    if (newValue) {
+      timerRef.current.timerId = setTimeout(() => {
+        const newSuggestions = FRUITS.filter((fruit) =>
+          fruit.toLocaleLowerCase().startsWith(newValue.toLocaleLowerCase())
+        );
+
+        setSuggestions([...newSuggestions]);
+      }, 1000);
+    } else setSuggestions([]);
+  };
+
+  const suggestionOnClick = (value:string) => {
+    setSuggestions([]);
+    setSearchInput(value);
+  };
+
+  const buildSuggestionsList = () => {
+    if (suggestions.length == 0) return <ul className="invisible "></ul>;
+    return (
+      <ul className="bg-white p-2 mt-1 bg-gray-100">
+        {suggestions.map((fruitName) => (
+          <li className="focus:bg-blue-400 p-2 border cursor-pointer" onClick={(e:any)=> suggestionOnClick(e.target.innerText)}>{fruitName}</li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="flex flex-col gap-5 items-center h-screen justify-center bg-gray-200">
+    <div className="flex flex-col gap-5 items-center h-screen justify-center bg-gray-200 font-mono">
       <h1 className="font-bold text-3xl text-orange-500">
         Search Any Fruit! 🍍🥭🍎🍉
       </h1>
-      <div>
-        <input className="border border-gray-800 w-[300px] p-1 rounded" />
-        <ul className="bg-white p-2 mt-1 bg-gray-100">
-          <li className="focus:bg-blue-400 p-2 border">Test</li>
-          <li>Test</li>
-        </ul>
+      <div className="">
+        <input
+          className="border border-gray-800 w-[300px] p-1 rounded"
+          value={searchInput}
+          onChange={(e) => textBoxOnChange(e.target.value)}
+        />
+        {buildSuggestionsList()}
       </div>
     </div>
   );
