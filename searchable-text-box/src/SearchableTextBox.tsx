96b2180c365f840d365d4ebbf6730aedd7062246
vengadeshraj <vengadeshraj405@gmail.com>
2026-03-06 22:26:25 +0530

    debounced moved to hook

--- searchable-text-box/src/SearchableTextBox.tsx
+++ searchable-text-box/src/SearchableTextBox.tsx
@@ -1,37 +1,20 @@
-import { useRef, useState } from "react";
 import { FRUITS } from "./constants";
+import useDebouncedSearch from "./hooks/useDebouncedSearch";
 
 export default function SearchableTextBox() {
-  const [searchInput, setSearchInput] = useState("");
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const timerRef: any = useRef({});
-
-  const textBoxOnChange = (newValue: string) => {
-    setSearchInput(newValue);
-    if (timerRef?.current?.timerId) clearTimeout(timerRef.current.timerId);
-
-    if (newValue) {
-      timerRef.current.timerId = setTimeout(() => {
-        const newSuggestions = FRUITS.filter((fruit) =>
-          fruit.toLocaleLowerCase().startsWith(newValue.toLocaleLowerCase())
-        );
-
-        setSuggestions([...newSuggestions]);
-      }, 1000);
-    } else setSuggestions([]);
-  };
-
-  const suggestionOnClick = (value:string) => {
-    setSuggestions([]);
-    setSearchInput(value);
-  };
+  const [inputValue, suggestions, onChange, reset] = useDebouncedSearch(FRUITS);
 
   const buildSuggestionsList = () => {
     if (suggestions.length == 0) return <ul className="invisible "></ul>;
     return (
       <ul className="bg-white p-2 mt-1 bg-gray-100">
         {suggestions.map((fruitName) => (
-          <li className="focus:bg-blue-400 p-2 border cursor-pointer" onClick={(e:any)=> suggestionOnClick(e.target.innerText)}>{fruitName}</li>
+          <li
+            className="focus:bg-blue-400 p-2 border cursor-pointer"
+            onClick={(e: any) => reset(e.target.innerText)}
+          >
+            {fruitName}
+          </li>
         ))}
       </ul>
     );
@@ -39,14 +22,12 @@ export default function SearchableTextBox() {
 
   return (
     <div className="flex flex-col gap-5 items-center h-screen justify-center bg-gray-200 font-mono">
-      <h1 className="font-bold text-3xl text-orange-500">
-        Search Any Fruit! 🍍🥭🍎🍉
-      </h1>
+      <h1 className="font-bold text-3xl text-orange-500">Search Any Fruit!</h1>
       <div className="">
         <input
           className="border border-gray-800 w-[300px] p-1 rounded"
-          value={searchInput}
-          onChange={(e) => textBoxOnChange(e.target.value)}
+          value={inputValue}
+          onChange={(e) => onChange(e.target.value)}
         />
         {buildSuggestionsList()}
       </div>
