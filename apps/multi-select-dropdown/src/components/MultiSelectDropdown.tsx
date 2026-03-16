0d4998557f0a2da1dcfe64bc7e62da1ed1e79dd6
vengadeshraj <vengadeshraj405@gmail.com>
2026-03-17 00:31:32 +0530

    few more changes addition

--- apps/multi-select-dropdown/src/components/MultiSelectDropdown.tsx
+++ apps/multi-select-dropdown/src/components/MultiSelectDropdown.tsx
@@ -6,9 +6,10 @@ const MultiSelectDropdown = (props: MultiSelectDropdownTyep) => {
   const { title } = props;
   return (
     <div className="flex flex-row gap-5 font-medium">
-      <label className=" text-2xl">{title + ""}</label>
-      <div className="border border-2 border-black rounded">
+      <label className="text-2xl">{title + ""}</label>
+      <div className="flex flex-row border border-2 border-black rounded">
         <input />
+        <button className="p-2">↓</button>
       </div>
     </div>
   );
