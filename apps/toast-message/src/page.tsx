714ed64759447afe2f6ec4b2ba339d9896b1e828
Vengadesh Raj <vengadeshraj@Vengadeshs-MacBook-Air.local>
2026-05-19 19:12:10 +0530

    toast message dev start

--- apps/toast-message/src/page.tsx
+++ apps/toast-message/src/page.tsx
@@ -1,5 +1,14 @@
-const Page =()=>{
-    return <div> Hello from toast message.</div>
+const Page = () => {
+    return (
+        <div className="flex flex-row h-screen bg-gray-200 gap-5">
+            <div>Contents will be here...</div>
+            <div>
+                <button>Success Toast</button>
+                <button>Failure Toast</button>
+                <button>Information Toast</button>
+                <button>Warning Toast</button>
+            </div>
+        </div>)
 }
 
 export default Page;
