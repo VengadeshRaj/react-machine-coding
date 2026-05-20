35ddfc55160240fc321c4e32ea6b2542317c0bdc
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-05-20 23:12:04 +0530

    new app toast message work around

---
+++ apps/toast-message/src/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="flex flex-row bg-green-200 h-screen items-center">
+      <div>Hello from Page...</div>
+    </div>
+  );
+};
+
+export default Page;
