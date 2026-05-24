9c0156334324a1dba67be84a7e5986c61d38ce53
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-05-24 12:32:20 +0530

    toast message readme update

--- apps/toast-message/src/hooks/useToastTimer.ts
+++ apps/toast-message/src/hooks/useToastTimer.ts
@@ -4,11 +4,16 @@ const useToastTimer = (event: () => void, isOpen: boolean) => {
   const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const DELAY = 2000;
 
+  const clerTimeOut =()=>{
+    if (timerRef.current) clearTimeout(timerRef.current);
+  }
+
   useEffect(() => {
     if (isOpen) {
-      if (timerRef.current) clearTimeout(timerRef.current);
+      clerTimeOut()
       timerRef.current = setTimeout(() => event(), DELAY);
     }
+    return clerTimeOut
   }, [isOpen, event]);
 };
 
