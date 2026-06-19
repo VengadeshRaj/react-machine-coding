3c023cf7f67e26ab9594fe7e3bc830c3f9c695dc
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-06-19 23:57:16 +0530

    new changes

--- apps/price-range/src/Page.tsx
+++ apps/price-range/src/Page.tsx
@@ -2,8 +2,8 @@ import PriceRange from "./components/PriceRange";
 
 const Page = () => {
   return (
-    <div className="flex flex-col h-screen items-center justify-center bg-green-400 p-5">
-      <h2>Price Range</h2>
+    <div className="flex flex-col h-screen items-center justify-center bg-green-400 p-5 gap-5">
+      <h2 className="text-2xl">Price Range</h2>
       <PriceRange />
     </div>
   );
--- apps/price-range/src/components/PriceRange.tsx
+++ apps/price-range/src/components/PriceRange.tsx
@@ -1,7 +1,8 @@
 const PriceRange = () => {
   return (
-    <div className="w-[100%]">
+    <div className="w-[100%] relative">
       <div className="bg-gray-300 h-2 rounded" />
+      <div className="absolute w-[15px] h-2 bg-blue-700 rounded-full cursor-pointer" draggable/>
     </div>
   );
 };
